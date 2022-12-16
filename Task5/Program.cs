e5c028a08ac1e7a9a7209ffc96639434a621e7ea
Natalya <07_nata@mail.ru>
2022-12-16 15:48:35 +0300

    Написали программу которая на выходе выводит последнюю цифру трехзначного числа

---
+++ Task5/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+Write("Введите число ");
+int n = int.Parse(ReadLine()!);
+if (n >= 100 && n <= 999) WriteLine(n%10);
+else WriteLine("ВВедено не трехзначное число");
