1c145698885acefb1e6650f68ec55f11e92d2961
Natalya <07_nata@mail.ru>
2022-12-16 14:48:53 +0300

    Решение задачи про дни недели

---
+++ Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+Write("Введите число ");
+int day = int.Parse(ReadLine());
+
+if (day <= 7 && day >= 1)
+{
+    if (day == 1)
+    {
+        Write("Понедельник");
+    }
+    if (day == 2)
+   {
+     Write("Вторник");
+   }
+   if (day == 3)
+    {
+        Write("Среда");
+    }
+    if (day == 4)
+   {
+     Write("Четверг");
+   }
+   if (day == 5)
+    {
+        Write("Пятница");
+    }
+    if (day == 6)
+   {
+     Write("Суббота");
+   }
+   if (day == 7)
+    {
+        Write("Воскресенье");
+    }
+}
+else
+{
+    Write("Данного дня не существует");
+}
