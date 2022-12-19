4c3a74f63057a385b6a4b5bbd9685f80efd2c25d
Natalya <07_nata@mail.ru>
2022-12-19 20:11:47 +0300

    задача 4

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+using static System.Console;
+Clear();
+
+Write("введите число: ");
+int num = int.Parse(ReadLine()!);
+
+int Count = 1;
+
+while (Count <= num)
+{
+    if (Count % 2 == 0)
+    {
+        Write("{0} ", Count);
+    }
+    Count ++;
+}
+
+WriteLine("");
