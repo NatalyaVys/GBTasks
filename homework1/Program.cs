fc63d887e1944d88cbf8eef29cf7e8cd9b941ff5
Natalya <07_nata@mail.ru>
2022-12-19 16:24:57 +0300

    исправила ошибку вывода

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using static System.Console;
+Clear();
+
+Write("введите первое число: ");
+int num_1 = int.Parse(ReadLine()!);
+
+Write("введите второе число: ");
+int num_2 = int.Parse(ReadLine()!);
+
+Write("введите третье число: ");
+int num_3 = int.Parse(ReadLine()!);
+
+int max = num_1;
+
+if (num_2 > max)
+{
+    max = num_2;
+}
+if ( num_3 > max)
+{
+    max = num_3;
+}
+WriteLine("max число: {0} ", max);
