281b7590bf0e65079c2ce342bbce615ab3cafdad
Natalya <07_nata@mail.ru>
2022-12-20 17:19:30 +0300

    Задача 12

--- Sem2/task3/Program.cs
+++ Sem2/task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 12:** Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 416, 4  -> кратно
+using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+int c = a%b;
+
+if (a%b == 0)
+{
+    WriteLine("Кратно");
+}
+else
+{
+    WriteLine("Некратно");
+    WriteLine(c);
+}
