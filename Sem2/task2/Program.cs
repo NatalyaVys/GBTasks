c2b4eda68afd57e87a5ac7c86dc8d233c9bdcf35
Natalya <07_nata@mail.ru>
2022-12-20 17:17:49 +0300

    Задача 11

--- Sem2/task2/Program.cs
+++ Sem2/task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46, 782 -> 72, 918 -> 98
+
+using static System.Console;
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+
+int a1 = number/100;
+int a2 = number/10;
+int a3 = number%10;
+int a4 = a1 * 10 + a3;
+
+WriteLine(a4);
