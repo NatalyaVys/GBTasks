942cfc4f2426f7b7e66ff30d000e238334ec6cd7
Natalya <07_nata@mail.ru>
2022-12-20 17:37:31 +0300

    задача 16

--- Sem2/task6/Program.cs
+++ Sem2/task6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **Задача 16:**Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a == b*b)
+{
+   WriteLine($"Число {a} явялется квардратом числа {b}"); 
+}
+else if (b == a*a)
+{
+   WriteLine($"Число {b} явялется квардратом числа {a}");
+}
+else
+{
+    WriteLine("Ни одно число не явялется квдратом другого");
+}
