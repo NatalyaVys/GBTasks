ef4b42c8cb173fca549e3d9fbda6608eb4927bb5
Natalya <07_nata@mail.ru>
2022-12-20 17:31:28 +0300

    доп.задача

--- Sem2/task4/Program.cs
+++ Sem2/task4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// доп задача. задача вводится два числа, и показывает либо больше, либо меньше, либо равно
+
+using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a>b)
+{
+    WriteLine(">");
+}
+else if (a<b)
+{
+    WriteLine("<");
+}
+else
+{
+    WriteLine("=");
+}
+
