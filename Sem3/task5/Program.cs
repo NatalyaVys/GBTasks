567e366af16c97a690b33dc19a1ba48159421fb4
Natalya <07_nata@mail.ru>
2022-12-23 15:31:46 +0300

    На входе число N, на выходе квадрат чисел от 1 до N

---
+++ Sem3/task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу квадратов чисел от 1 до N.
+
+using static System.Console;
+
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    WriteLine(count * count);
+    count++;
+}
