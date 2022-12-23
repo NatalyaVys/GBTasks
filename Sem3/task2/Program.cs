4ec83e3b8ec89946d33e9df289860ac14032a06b
Natalya <07_nata@mail.ru>
2022-12-23 15:10:04 +0300

    по номеру дня недели, выводит его название

---
+++ Sem3/task2/Program.cs
@@ -0,0 +1,51 @@
+﻿//По номеру дня недели вывести его название
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(ReadLine());
+
+switch(day)
+{
+    case 1:
+        {
+            WriteLine("Понедельник");
+            break;
+        }
+    case 2:
+        {
+            WriteLine("Вторник");
+            break;
+        }
+    case 3:
+        {
+            WriteLine("Среда");
+            break;
+        }
+    case 4:
+        {
+            WriteLine("Четверг");
+            break;
+        }
+    case 5:
+        {
+            WriteLine("Пятница");
+            break;
+        }
+    case 6:
+        {
+            WriteLine("Суббота");
+            break;
+        }
+    case 7:
+        {
+            WriteLine("Воскресенье");
+            break;
+        }
+    default:
+        {
+            WriteLine("Это не день недели");
+            break;
+        }
+}
