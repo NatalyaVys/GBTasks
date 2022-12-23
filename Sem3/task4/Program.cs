5702ae9c07e61621eb355e5151c40148dab43b06
Natalya <07_nata@mail.ru>
2022-12-23 15:25:04 +0300

    Задача про токарей

---
+++ Sem3/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача про токарей.
+
+using static System.Console;
+
+WriteLine("Введите количество изготовленных деталей: ");
+int number = Convert.ToInt32(ReadLine());
+
+int x = 0;
+int y = 0;
+
+// x = 2(y +y)
+
+WriteLine(number % 6 == 0 && number > 0? $"Токарь сделал {number / 3 * 2}, Полутокари сделали по {number / 6}" : "Токарь ошибся");
