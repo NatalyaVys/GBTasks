f93fad5970e6d89b81d02ec8233be66d262f33c4
Natalya <07_nata@mail.ru>
2022-12-24 15:01:25 +0300

    Задача 23, на входе число N, на выходе таблица кубов чисел от 1 до N

---
+++ DZ-Sem3/dz-zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    WriteLine(count * count*count);
+    count++;
+}
