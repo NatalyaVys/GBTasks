0304c6ba132850d0d3703853984b54f921d18d9a
Natalya <07_nata@mail.ru>
2022-12-24 15:31:22 +0300

    Задача 19,пятизначное  число является ли  палиндром?

---
+++ DZ-Sem3/dz-zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+ // является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int a = number / 10000;
+int b = (number / 1000) % 10;
+int c = (number / 10) % 10;
+int d = number % 10;
+
+if (a == d && b == c)
+{
+    WriteLine("Число " + number + " является палиндромом");
+}
+else
+{
+    WriteLine("Число " + number + " не является палиндромом");
+}
