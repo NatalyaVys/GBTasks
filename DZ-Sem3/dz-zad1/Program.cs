f8e8b082a8d9dd3cfef332d51b16fb81fc1b7ecd
Natalya <07_nata@mail.ru>
2022-12-24 14:52:23 +0300

    Задача 21, на входе коорд 2х точек, находит расстояние между ними в 3D

---
+++ DZ-Sem3/dz-zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53.
+
+
+using static System.Console;
+
+WriteLine("Введите ax: ");
+int ax = int.Parse(ReadLine()!);
+WriteLine("Введите ay: ");
+int ay = int.Parse(ReadLine()!);
+WriteLine("Введите az: ");
+int az = int.Parse(ReadLine()!);
+WriteLine("Введите bx: ");
+int bx = int.Parse(ReadLine()!);
+WriteLine("Введите by: ");
+int by = int.Parse(ReadLine()!);
+WriteLine("Введите bz: ");
+int bz = int.Parse(ReadLine()!);
+
+double x = bx - ax;
+double y = by - ay;
+double z = az - bz;
+
+double line = Math.Sqrt(Math.Pow(x,2) + Math.Pow(y,2)+ Math.Pow(z,2));
+
+WriteLine($"{line:f2}");
