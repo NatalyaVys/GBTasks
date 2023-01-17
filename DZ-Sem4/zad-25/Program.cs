af336c1940e97b400d0ca26659a1c5ba0fe4fa13
Natalya <07_nata@mail.ru>
2023-01-17 10:32:48 +0300

    программа возводит А в степень В

---
+++ DZ-Sem4/zad-25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите программу, реализующую метод,который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B с использованием цикла.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+using static System.Console;
+
+WriteLine("введите число A");
+int A = int.Parse(ReadLine()!);
+WriteLine("введите число b");
+int b = int.Parse(ReadLine()!);
+int stepen = A;
+
+for (int i = 1; i < b; i++)
+{
+stepen = stepen * A;
+}
+WriteLine("A в степени b равно: " + stepen);
