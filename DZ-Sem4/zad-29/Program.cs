34b2c745d716dece027fbe449438caa4fbbb96dc
Natalya <07_nata@mail.ru>
2023-01-17 10:55:06 +0300

    программа формирует массив из случайных чисел и выводит на экран

---
+++ DZ-Sem4/zad-29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 29: Напишите программу, реализующую метод, который формирует массив случайных
+//целых чисел из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19,6, 1, 33 -> [1, 2, 5, 7, 19,6, 1, 33 ]
+
+using static System.Console;
+
+int lenArr = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArr];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
+int ReadInt(string message)      // 
+{
+    WriteLine(message );
+    return Convert.ToInt32(Console.ReadLine());
+}
