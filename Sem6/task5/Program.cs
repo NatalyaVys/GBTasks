7b0b43f5f4ff9df6862e7c3f37944f88b4e6a242
Natalya <07_nata@mail.ru>
2023-01-20 16:11:07 +0300

    фибоначи

---
+++ Sem6/task5/Program.cs
@@ -0,0 +1,19 @@
+﻿// N Число фибоначи(ряд) без рекурсии
+
+using static System.Console;
+Clear();
+
+WriteLine($"ряд фибоначи = [{string.Join(",", fibonachi(20))}]");
+//{string.Join(",", array)
+
+int[] fibonachi(int N)
+{
+    int[] result = new int[N];
+    result[0] = 0;
+    result[1] = 1;
+    for(int i = 2; i < N; i ++)
+    {
+        result[i] = result[i-1] + result[i-2];
+    }
+return result;
+}
