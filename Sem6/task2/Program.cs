a429425dc64616683c73be68b599bd471920997b
Natalya <07_nata@mail.ru>
2023-01-20 14:59:56 +0300

    Задача 40, на входе три числа, проверяет может ли существовать такой треугольник

---
+++ Sem6/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿//задача 40. программа на вход принимает на вход три числа 
+//и проверяет, может ли существовать треугольник со сторонами такой длины.
+
+using static System.Console;
+
+//треугольник abc
+
+int a = askNumber("введите сторону a: ");
+int b = askNumber("введите сторону b: ");
+int c = askNumber("введите сторону c: ");
+
+WriteLine(IsTriangle(a,b,c) ? $"Треугольник со сторонами {a}, {b}, {c} существует." : $"Треугольник со сторонами {a}, {b}, {c} не существует.");
+
+bool IsTriangle(int inA, int inB, int inC)
+{
+    if (a+b> c && a+c >b && b+c>a)  return true;
+    return false;
+}
+
+
+int askNumber(string inPrompt)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while(!int.TryParse(userInput, out resultNum))
+    {
+        Console.Write(inPrompt);
+        userInput= Console.ReadLine()!;
+    }
+    return resultNum;
+}
