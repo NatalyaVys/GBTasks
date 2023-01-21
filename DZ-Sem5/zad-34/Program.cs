0c3c3ec4e6d679827520099f086f6e6de42fb593
Natalya <07_nata@mail.ru>
2023-01-21 13:09:38 +0300

    Задача 34, формирует массив и выдает сколько четных чисел в массиве

---
+++ DZ-Sem5/zad-34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Напишите программу реализующую методы формирования массива, 
+// заполненного случайными положительными трёхзначными числами, и подсчета количества чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+int count = 0;
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] % 2 == 0)
+count++;
+}
+WriteLine($"в данном массиве, {count} четных. ");
+
+
+void PrintArray(int[] numbers)
+{
+    Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Write("]");
+    WriteLine();
+}
