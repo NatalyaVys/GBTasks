1ab19cf4998a6b8565066ae26bbe9de83869437e
Natalya <07_nata@mail.ru>
2023-01-22 16:05:20 +0300

    задача 41, сколько чисел больше нуля ввел пользователь

---
+++ DZ-Sem6/zad-41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int Numbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+WriteLine($"Количество чисел больше нуля: {Numbers(array)}");
