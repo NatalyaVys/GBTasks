4c0f1622975ae8d15048e7aa6120862f6d5b7445
Natalya <07_nata@mail.ru>
2023-01-23 20:26:42 +0300

    зад.46, двумерный массив

---
+++ Sem7/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// **Задача 46:** Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+//                 Например: m = 3, n = 4.     1 4 8 19        5 -2 33 -2       77 3 8 1
+
+using static System.Console;
+Clear();
+Write("Введите размер матрицы и диапозон значений через пробел: ");
+string[] parameters = ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
+PrintMatrixArray(array);
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rand = new Random();
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i,j] = rand.Next(minValue, maxValue);
+        }
+    }
+    return resultArray;
+}
+
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");  // вывод в 3 символа
+        }
+        WriteLine();
+    }
+}
+
+
+
