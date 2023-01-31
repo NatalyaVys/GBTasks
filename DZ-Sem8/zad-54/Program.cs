8a5cb47a0d8ab254d298b76a548046a38e7e02a9
Natalya <07_nata@mail.ru>
2023-01-31 15:37:31 +0300

    задача 54. двумерный массив и упорядочевание строк по убыванию

--- DZ-Sem8/zad-54/Program.cs
+++ DZ-Sem8/zad-54/Program.cs
@@ -9,8 +9,8 @@
 // 8 4 4 2
 
 using static System.Console;
- Clear();
- 
+Clear();
+
 WriteLine("введите количество строк:");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 WriteLine("введите количество столбцов:");
@@ -21,48 +21,48 @@ FillArrayRandomNumbers(numbers);
 
 WriteLine("введеный массив:");
 PrintArray(numbers);
-            
-    for (int a = 0; a < numbers.GetLength(0); a++)
+
+for (int a = 0; a < numbers.GetLength(0); a++)
 {
     for (int b = 0; b < numbers.GetLength(1) - 1; b++)
     {
-    for (int c = 0; c < numbers.GetLength(1) - 1; c++)
-      {
-    if (numbers[a, c] < numbers[a, c + 1]) 
-    {
-         int temp = 0;
-        temp = numbers[a, c];
-        numbers[a, c] = numbers[a, c + 1];
-        numbers[a, c + 1] = temp;
-       }
-      }
+        for (int c = 0; c < numbers.GetLength(1) - 1; c++)
+        {
+            if (numbers[a, c] < numbers[a, c + 1])
+            {
+                int temp = 0;
+                temp = numbers[a, c];
+                numbers[a, c] = numbers[a, c + 1];
+                numbers[a, c + 1] = temp;
+            }
+        }
     }
 }
 
 WriteLine("массив с упорядоченными значениями:");
 PrintArray(numbers);
-            
+
 void FillArrayRandomNumbers(int[,] array)
- {
+{
     for (int a = 0; a < array.GetLength(0); a++)
     {
         for (int b = 0; b < array.GetLength(1); b++)
         {
-             array[a, b] = new Random().Next(0, 10);
-            }
+            array[a, b] = new Random().Next(0, 10);
         }
     }
+}
 
 void PrintArray(int[,] array)
-    {
+{
     for (int a = 0; a < array.GetLength(0); a++)
-     {
+    {
         Write("[ ");
         for (int b = 0; b < array.GetLength(1); b++)
         {
-        Write(array[a, b] + " ");
+            Write(array[a, b] + " ");
         }
-    Write("]");
-    WriteLine("");
+        Write("]");
+        WriteLine("");
     }
- }
+}
