b111ca0ff075fac7cbb9f468a4494f65c83ed4ee
Natalya <07_nata@mail.ru>
2023-02-04 15:04:18 +0300

    задача 54, выводит все натур.числа от N до 1

---
+++ DZ-Sem9/zad 64/Program.cs
@@ -0,0 +1,22 @@
+﻿//*Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(ReadLine()!);
+
+
+void NumberCounter (int number)
+{
+    if (number < 0) Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+NumberCounter(number);
+WriteLine();
